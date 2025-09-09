ee6ff8884588c948ccbe167e05aefa7a1d763b0f
Bashira Akter Anima <bashiraakteranima@Bashiras-MacBook-Air.local>
2025-09-09 15:57:29 -0700

    update

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main(){
     string temp[3] = {"cream", "corn", "natural flavorings"};
     LabelInformation creamedCorn(temp, 120, 3);
+    cout << "Compiled and Ran Successfully! Submit the Screenshot of the output terminal in your quiz." << endl;
 
     return 0;
 }
