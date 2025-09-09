c298e14eb2b00b3cbf0a4af80e974bdfe339d735
Bashira Akter Anima <bashiraakteranima@Bashiras-MacBook-Air.local>
2025-09-09 16:01:15 -0700

    update

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 int main(){
     string temp[3] = {"cream", "corn", "natural flavorings"};
     LabelInformation creamedCorn(temp, 120, 3);
-    cout << "Compiled and Ran Successfully! Submit the Screenshot of the output terminal in your quiz." << endl;
+    cout << "Compiled and Ran Successfully!" << endl;
+    cout << "Submit the Screenshot of the output terminal in your quiz." << endl;
+    cout << "Not git push required!"<<endl;
 
     return 0;
 }
